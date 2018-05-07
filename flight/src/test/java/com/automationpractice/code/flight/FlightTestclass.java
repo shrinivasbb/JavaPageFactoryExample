138ee7af7d26e4e5bc55a1f36676083d453c3749
shrinivasbb <shrinivasbagewadi@gmail.com>
2018-05-07 22:16:17 +0530

    Added chrome maximize workaround

--- flight/src/test/java/com/automationpractice/code/flight/FlightTestclass.java
+++ flight/src/test/java/com/automationpractice/code/flight/FlightTestclass.java
@@ -10,6 +10,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
